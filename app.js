709aa012f8cc604233366f125a22c4345ef3afbe
Michael Zaleski <me@mikezaleski.com>
2021-03-31 07:26:58 -0400

    implement basic routing: index, new, and show routes

--- app.js
+++ app.js
@@ -21,15 +21,31 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true}));
+
 
 app.get('/', (req, res) => {
   res.render('home')
 }) 
 
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({title: 'My Backyard', description: 'Cheap Camping!' })
-  await camp.save();
-  res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', {campgrounds});
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', {campground});
 })
 
 app.listen(3000, () => {
