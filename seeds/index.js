5dadb2da20c5ad566a3965ae375e52caea0ff33e
Michael Zaleski <me@mikezaleski.com>
2021-04-02 15:37:57 -0400

    add logic to get a unique image for each campground running seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { tall } = require('tall');
 const cities = require('./cities');
 const {descriptors, places} = require('./seedHelpers')
 const Campground = require('../models/campground');
@@ -15,6 +16,23 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getImage() {
+  const unsplashUrl = 'https://source.unsplash.com/collection/483251';
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  try {
+    await delay(1000);
+    return await tall(unsplashUrl);
+  } catch (err) {
+    console.error('AAAW 👻', err);
+  }
+}
+
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
@@ -22,10 +40,13 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 30) + 10;
+    const image = await getImage();
+    console.log(image)
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
+      // image: 'https://source.unsplash.com/collection/483251',
+      image: `${image}`,
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus omnis hic recusandae aspernatur rem aliquam consectetur, iste, aperiam sapiente quisquam reiciendis culpa adipisci dolorem sed odit officiis a similique corrupti.',
       price //same as price: price
     })
