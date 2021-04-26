2c744ced951f2f03c91a3b76b06865917e3b4a3b
Michael Zaleski <me@mikezaleski.com>
2021-04-26 05:50:43 -0400

    add flash messages for errors

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -33,6 +33,10 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id).populate('reviews');
+  if (!campground) {
+    req.flash('error', 'The requested campground does not exist');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/show', {campground});
 }))
 
