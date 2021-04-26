9373994f00274020185aa123baad5524cd6e1f76
Michael Zaleski <me@mikezaleski.com>
2021-04-26 07:42:40 -0400

    - authentication:   - add register and login routes and forms   - add isLoggedIn middleware to protect routes   - require login for new, edit, and delete campgrounds routes

---
+++ routes/users.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/user')
+
+router.get('/register', (req, res) => {
+  res.render('users/register')
+});
+
+router.post('/register', catchAsync(async (req, res) => {
+  try {
+    const {username, email, password } = req.body;
+    const user = new User({username, email});
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    req.flash('success', 'Welcome to YelpCamp!');
+    res.redirect('/campgrounds');
+  } catch (e) {
+    req.flash('error', e.message)
+    res.redirect('/register')
+  }
+}));
+
+router.get('/login', (req, res) => {
+  res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+  req.flash('success', 'Welcome back!');
+  res.redirect('/campgrounds');
+})
+
+module.exports = router;
