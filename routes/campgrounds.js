0a1a7ac733230275a09c7285887879bd15f7a051
Michael Zaleski <me@mikezaleski.com>
2021-04-26 21:52:17 -0400

    Fix campground PUT route to use findIdAndUpdate, only allow updating if currently signed in user is author

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -49,7 +49,15 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
 
 router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
   const {id} = req.params;
-  const campground = await Campground.findOneAndUpdate(id, {...req.body.campground});
+  const campground = await Campground.findById(id);
+  console.log('id: ', id);
+  console.log('campground: ', campground);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do that');
+    return res.redirect(`/campgrounds/${id}`)
+  }
+  const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+  console.log(camp)
   req.flash('success', 'Successfully updated campground');
   res.redirect(`/campgrounds/${campground._id}`)
 }))
